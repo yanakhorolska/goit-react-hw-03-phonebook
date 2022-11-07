7a280a342ee2a106576daf2e9e0405f5de7b2363
yanakhorolska <yana.khorolska@protonmail.com>
2022-11-07 19:21:26 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Harry Potter', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Granger', number: '443-89-12' },
-      { id: 'id-3', name: 'Ron Weasley', number: '645-17-79' },
-      { id: 'id-4', name: 'Albus Dumbledore', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
   addContact = ({ name, number }) => {
@@ -32,14 +27,30 @@ class App extends Component {
     }));
     return true;
   };
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
   filterChanger = e => {
     this.setState({ filter: e.currentTarget.value });
   };
+
   deleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
   render() {
     const visibleContacts = this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
